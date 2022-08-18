958c3d6a3befab3af3f615fd210c76691978de00
Valentin <valentin.lyash@gmail.com>
2022-08-18 18:14:40 -0400

    edited the image linked using imgur upload storage and then linked inside placeholders.js. Then edited inside Hero.jsx to display only primary image and removed secondary display of image. index.css was edited to remove underline under the header and the header was changed from dark to light theme. Inside Header.client.js also changed the dark shadow  - shadow-darkHeader effect, for mobileheader and desktopheader components

--- src/lib/placeholders.js
+++ src/lib/placeholders.js
@@ -14,13 +14,13 @@ const PLACEHOLDERS = {
           mediaContentType: 'IMAGE',
           alt: 'Tracks in the snow leading to a person on a mountain top with a red jacket contrasting to an epic blue horizon with a mountain range in the distance.',
           previewImage: {
-            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/Hydrogen_Hero_Feature_1.jpg?v=1654902468',
+            url: 'https://i.imgur.com/q6WyS9T.jpg',
           },
           id: 'gid://shopify/MediaImage/29259478466616',
           image: {
-            url: 'https://cdn.shopify.com/s/files/1/0551/4566/0472/files/Hydrogen_Hero_Feature_1.jpg?v=1654902468',
-            width: 2500,
-            height: 3155,
+            url: 'https://i.imgur.com/q6WyS9T.jpg',
+            width: 2000,
+            height: 1155,
           },
         },
       },
